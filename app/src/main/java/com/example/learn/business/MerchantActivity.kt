a767127fbad0917712780905a1035f5101139551
Rodrigo <rvidela@outlook.com>
2022-04-14 20:10:20 -0300

    MerchantActivity migrated to view binding.

--- app/src/main/java/com/example/learn/business/MerchantActivity.kt
+++ app/src/main/java/com/example/learn/business/MerchantActivity.kt
@@ -3,21 +3,26 @@ package com.example.learn.business
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.learn.R
+import com.example.learn.databinding.MerchantActivityBinding
 import com.example.learn.log
-import kotlinx.android.synthetic.main.merchant_activity.*
 
 @SuppressLint("Registered")
 class MerchantActivity : AppCompatActivity() {
 
+    private lateinit var binding: MerchantActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.merchant_activity)
+
+        binding = MerchantActivityBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
         initialView()
         log("MerchantActivity onCreate")
     }
 
     private fun initialView() {
-        layContent.laySettle = laySettle
+        binding.layContent.laySettle = binding.laySettle
     }
 }
