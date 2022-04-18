ec278dde6a1e7a925752905fd7ff26833970ddb8
Rodrigo <rvidela@outlook.com>
2022-04-18 12:25:52 -0300

    More layouts converted to view binding.

--- app/src/main/java/com/example/learn/business/MerchantTitleLayout.kt
+++ app/src/main/java/com/example/learn/business/MerchantTitleLayout.kt
@@ -11,10 +11,10 @@ import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.FrameLayout
 import com.example.learn.R
 import com.example.learn.argbEvaluator
+import com.example.learn.databinding.MerchantTitleLayoutBinding
 import com.example.learn.dp
 import com.example.learn.resDimension
 import com.example.learn.resDrawable
-import kotlinx.android.synthetic.main.merchant_title_layout.view.*
 
 class MerchantTitleLayout(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
     private val adInterpolator = AccelerateDecelerateInterpolator()
@@ -27,9 +27,8 @@ class MerchantTitleLayout(context: Context, attrs: AttributeSet?) : FrameLayout(
     private val drawableTogether = resDrawable(R.mipmap.icon_together)
     private val drawableMenu = resDrawable(R.mipmap.icon_menu)
 
-    init {
-        LayoutInflater.from(context).inflate(R.layout.merchant_title_layout, this)
-    }
+    private var binding: MerchantTitleLayoutBinding =
+        MerchantTitleLayoutBinding.inflate(LayoutInflater.from(context), this, true)
 
     fun effectByOffset(dy: Int): Float {
         if (dy > 0 && offset == offsetMax) return 1F
@@ -44,20 +43,22 @@ class MerchantTitleLayout(context: Context, attrs: AttributeSet?) : FrameLayout(
         setBackgroundColor(argbEvaluator.evaluate(effect, Color.TRANSPARENT, 0xFFFAFAFA.toInt()) as Int)
 
         val e: Int = argbEvaluator.evaluate(effect, Color.WHITE, 0xFF646464.toInt()) as Int
-        vBack.setImageDrawable(tintDrawable(drawableBack, ColorStateList.valueOf(e)))
-        vTogether.setImageDrawable(tintDrawable(drawableTogether, ColorStateList.valueOf(e)))
-        vMenu.setImageDrawable(tintDrawable(drawableMenu, ColorStateList.valueOf(e)))
-        vCollection.setImageDrawable(tintDrawable(drawableCollection, ColorStateList.valueOf(e)))
-        vSearch.setImageDrawable(tintDrawable(drawableSearch, ColorStateList.valueOf(e)))
+        binding.apply {
+            vBack.setImageDrawable(tintDrawable(drawableBack, ColorStateList.valueOf(e)))
+            vTogether.setImageDrawable(tintDrawable(drawableTogether, ColorStateList.valueOf(e)))
+            vMenu.setImageDrawable(tintDrawable(drawableMenu, ColorStateList.valueOf(e)))
+            vCollection.setImageDrawable(tintDrawable(drawableCollection, ColorStateList.valueOf(e)))
+            vSearch.setImageDrawable(tintDrawable(drawableSearch, ColorStateList.valueOf(e)))
 
-        vSearch.scaleX = (1 - 0.4 * effect).toFloat()
-        vSearch.scaleY = (1 - 0.4 * effect).toFloat()
-        vSearch.translationX = -(vSearchBorder.width - vSearch.width + dp(3)) * effect
-        vSearchBorder.alpha = effect
-        vSearchBorder.pivotX = vSearchBorder.width.toFloat()
-        vSearchBorder.scaleX = (0.2 + 0.8 * effect).toFloat()
-        vSearchHint.alpha = effect
-        vSearchHint.translationX = (vSearchHint.width / 3) * (1 - effect)
+            vSearch.scaleX = (1 - 0.4 * effect).toFloat()
+            vSearch.scaleY = (1 - 0.4 * effect).toFloat()
+            vSearch.translationX = -(vSearchBorder.width - vSearch.width + dp(3)) * effect
+            vSearchBorder.alpha = effect
+            vSearchBorder.pivotX = vSearchBorder.width.toFloat()
+            vSearchBorder.scaleX = (0.2 + 0.8 * effect).toFloat()
+            vSearchHint.alpha = effect
+            vSearchHint.translationX = (vSearchHint.width / 3) * (1 - effect)
+        }
 
         return effect
     }
